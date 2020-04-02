83170512022b823d9c813b348223e66fc1e934b7
Shalitha Pathiranage <shali96shri@gmail.com>
2020-04-02 23:55:11 +0530

    ionic and angular

---
+++ src/providers/promotion/promotion.ts
@@ -0,0 +1,41 @@
+import { Http, Response } from '@angular/http';
+import { Injectable } from '@angular/core';
+import {Promotion}  from '../../shared/promotion';
+import {Observable} from 'rxjs/Observable';
+import {baseURL} from '../../shared/baseurl';
+import {ProcessHttpmsgProvider} from '../process-httpmsg/process-httpmsg';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/delay';
+import 'rxjs/add/operator/catch';
+/*
+  Generated class for the PromotionProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class PromotionProvider {
+
+  constructor(public http: Http,  private processHttpmesgService: ProcessHttpmsgProvider)
+   {
+    console.log('Hello PromotionProvider Provider');
+  }
+  getPromotions(): Observable<Promotion[]>{
+    return this.http.get(baseURL + 'promotions')
+    .map(res => { return this.processHttpmesgService.extractData(res)})
+    .catch(error => {return this.processHttpmesgService.handleError(error)})
+    }
+    getPromotion(id:number): Observable<Promotion> {
+      return this.http.get(baseURL + 'promotions/'+ id)
+      .map(res => { return this.processHttpmesgService.extractData(res)})
+      .catch(error => {return this.processHttpmesgService.handleError(error)})
+      
+    }
+    getFeaturedPromotion(): Observable<Promotion> {
+      return this.http.get(baseURL + 'promotions?featured=true')
+      .map(res => { return this.processHttpmesgService.extractData(res)[0]})
+      .catch(error => {return this.processHttpmesgService.handleError(error)})
+      
+    }
+
+}
